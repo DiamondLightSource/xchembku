7646a11dfeb6ed6be529d64f5903411fdb606bde
David Erb <david.erb@diamond.ac.uk>
2023-04-14 06:41:51 +0000

    fixes upsert droplocation

--- src/xchembku_lib/datafaces/direct_crystal_well_droplocations.py
+++ src/xchembku_lib/datafaces/direct_crystal_well_droplocations.py
@@ -1,5 +1,9 @@
+import copy
 import logging
 from typing import Dict, List
+
+from dls_normsql.constants import CommonFieldnames
+from dls_utilpack.describe import describe
 
 from xchembku_api.models.crystal_well_droplocation_model import (
     CrystalWellDroplocationModel,
@@ -56,36 +60,68 @@ class DirectCrystalWellDroplocations(DirectBase):
     ) -> Dict:
         """
         Caller provides the crystal well droplocation record with the fields to be updated.
+
+        We don't insert for the same crystal_well_uuid twice.
+
+        TODO: Find more efficient way to upsert_crystal_well_droplocations in batch.
         """
 
         if why is None:
             why = "upsert_crystal_well_droplocations"
 
-        # We're being given models, so serialize them into dicts to give to the sql.
-        records = [model.dict() for model in models]
+        inserted_count = 0
+        updated_count = 0
 
-        count = 0
-        new_records = []
-        for record in records:
-            result = await self.update(
-                "crystal_well_droplocations",
-                record,
-                # We upsert the droplocation record keyed on the crystal well uuid.
-                "(crystal_well_uuid = ?)",
-                subs=[record["crystal_well_uuid"]],
+        # Loop over all the models to be upserted.
+        for model in models:
+            # Find any existing record for this model object.
+            records = await self.query(
+                "SELECT * FROM crystal_well_droplocations WHERE crystal_well_uuid = ?",
+                subs=[model.crystal_well_uuid],
                 why=why,
             )
-            if result.get("count", 0) == 0:
-                new_records.append(record)
 
-            count += result.get("count", 0)
+            if len(records) > 0:
+                logger.debug(
+                    describe(
+                        "crystal_well_droplocation record before update", records[0]
+                    )
+                )
+                # Make a copy of the model record and remove some fields not to update.
+                model_dict = copy.deepcopy(model.dict())
+                model_dict.pop(CommonFieldnames.UUID)
+                model_dict.pop(CommonFieldnames.CREATED_ON)
+                model_dict.pop("crystal_well_uuid")
+                result = await self.update(
+                    "crystal_well_droplocations",
+                    model_dict,
+                    "(crystal_well_uuid = ?)",
+                    subs=[model.crystal_well_uuid],
+                    why=why,
+                )
+                updated_count += result.get("count", 0)
 
-        await self.insert(
-            "crystal_well_droplocations",
-            new_records,
-            why=why,
-        )
+                # Find any existing record for this model object.
+                records = await self.query(
+                    "SELECT * FROM crystal_well_droplocations WHERE crystal_well_uuid = ?",
+                    subs=[model.crystal_well_uuid],
+                    why=why,
+                )
+                logger.debug(
+                    describe(
+                        "crystal_well_droplocation record after update", records[0]
+                    )
+                )
 
-        count += len(new_records)
+            else:
+                await self.insert(
+                    "crystal_well_droplocations",
+                    [model.dict()],
+                    why=why,
+                )
+                inserted_count += 1
 
-        return {"count": count}
+        return {
+            "updated_count": updated_count,
+            "inserted_count": inserted_count,
+        }
