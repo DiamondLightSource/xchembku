ef76b2d068c457a85e55907b6c915f31f26144a6
David Erb <david.erb@diamond.ac.uk>
2023-05-02 07:59:08 +0100

    adds normalize_subwell_name

--- src/xchembku_lib/crystal_plate_objects/swiss3.py
+++ src/xchembku_lib/crystal_plate_objects/swiss3.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from typing import Dict, Optional, Tuple
 
 # Base class for generic things.
@@ -27,6 +28,33 @@ class Swiss3(Thing):
     # ----------------------------------------------------------------------------------------
     def get_well_count(self) -> int:
         return self.__WELL_COUNT
+
+    # ----------------------------------------------------------------------------------------
+    def normalize_subwell_name(self, subwell_name: str) -> str:
+        """
+        Converts the name given to the subwell by Luigi into the format expected by the beamline.
+
+        Args:
+            subwell_name (str): stem part of the subwell's image filename
+
+        Raises:
+            ValueError: the value does not follow the convention
+
+        Returns:
+            str: the displayable subwell position
+        """
+        sub_letters = "acd"
+        pattern = re.compile(r"^(\w{4})_([0-1][0-9])([A-H])_([123])$")
+        match = re.findall(pattern, subwell_name)
+        if match:
+            bar, row, col, sub = match[0]
+            sub = sub_letters[int(sub) - 1]
+            position = f"{col}{row}{sub}"
+            return position
+        else:
+            raise ValueError(
+                f'subwell_name "{subwell_name}" is not of the expected format'
+            )
 
     # ----------------------------------------------------------------------------------------
     def compute_drop_location_microns(
